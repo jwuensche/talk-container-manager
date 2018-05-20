267005cf4273132348f2985595db8a161e3bd001
Fin Christensen <christensen.fin@gmail.com>
2018-05-20 16:43:22 +0200

    Add proper proxy of wetty containers

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const apiProxy = httpProxy.createProxyServer();
 // TODO:
 //  - handle container exit
 //  - dynamic container name
-//
+
 const wetty = {
     Image: 'git-wetty',
     name: 'git-wetty',
@@ -38,9 +38,8 @@ function forward(req, res, user, status) {
     const ip = status.data.NetworkSettings.IPAddress;
     console.log('Waiting for container for user', user, 'to become available...');
     waitForPort(ip, 4123, (err) => {
-        console.log('Container for user', user, 'is now running!:', ip, req.url);
+        console.log('Container for user', user, 'is now running!');
         req.url = req.params['1'];
-	console.log('New url:', req.url);
         apiProxy.web(req, res, {
             target: 'http://' + ip + ':4123',
         });
@@ -50,8 +49,12 @@ function forward(req, res, user, status) {
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
 
-const handler = (req, res, next) => {
-    console.log(req.params);
+function handler(req, res, next) {
+    if (shutdownRunning) {
+        res.status(404).send('Not found');
+        return;
+    }
+
     let user = req.params['0'];
     console.log(user);
     if (users.has(user)) {
@@ -66,8 +69,44 @@ const handler = (req, res, next) => {
             .then(container => container.status())
             .then(forward.bind(null, req, res, user));
     }
-};
+}
 
-app.all(/^\/terminal\/([a-zA-Z0-9\-]*)(.*)$/, handler);
+app.all(/^\/terminal\/([^/]*)(.*)$/, handler);
+app.all(/^\/wetty\/.*$/, (req, res, next) => {
+    if (shutdownRunning) {
+        res.status(404).send('Not found');
+        return;
+    }
+
+    console.log(req.url, req.headers.referer);
+
+    if (users.size <= 0) {
+        res.status(404).send('Not found');
+        return;
+    }
+
+    let initiator;
+    if (!req.headers.referer) {
+        initiator = users.keys().next().value;
+    } else {
+        initiator = decodeURI(/^http.*\/\/.*\/terminal\/([^/]*).*$/.exec(req.headers.referer)['1']);
+    }
+    const container = users.get(initiator);
+
+    if (!container) {
+        res.status(404).send('Not found');
+        return;
+    }
+
+    container.status()
+        .then(status => {
+            const ip = status.data.NetworkSettings.IPAddress;
+            waitForPort(ip, 4123, (err) => {
+                apiProxy.web(req, res, {
+                    target: 'http://' + ip + ':4123',
+                });
+            });
+        });
+});
 
 app.listen(8000);
